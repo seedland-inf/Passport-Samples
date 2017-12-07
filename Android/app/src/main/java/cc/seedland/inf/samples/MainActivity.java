db639282409aff885da6306443db4d828f465db5
徐春蕾 <xuchunlei@xuchunleideMacBook-Pro.local>
2017-12-07 15:12:00 +0800

    完成Demo

--- Android/app/src/main/java/cc/seedland/inf/samples/MainActivity.java
+++ Android/app/src/main/java/cc/seedland/inf/samples/MainActivity.java
@@ -1,11 +1,9 @@
 package cc.seedland.inf.samples;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
